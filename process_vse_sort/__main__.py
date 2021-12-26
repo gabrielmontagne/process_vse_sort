21d6c38fa62b21f1b9b33ba897dd58daacd27dac
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2021-12-26 23:16:35 +0100

    cleanup

--- process_vse_sort/__main__.py
+++ process_vse_sort/__main__.py
@@ -1,12 +1,16 @@
 from xyy import process_frame, start_server
 
+
 @process_frame
-def sort_pixels(pixs, input_path):
-    print('sort pixels', pixs, input_path)
+def sort_pixels(pixs, config):
+    pixs.sort(axis=0)
+    return pixs
+
 
 def main():
-    print('running module process_vse_sort')
+    print('Starting process_vse_sort server')
     start_server()
+
 
 if __name__ == '__main__':
     main()
